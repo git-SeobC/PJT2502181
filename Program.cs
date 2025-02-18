f9c532f6e9493acf82f9a936f907affe2116b272
SBC <seobc789@gmail.com>
2025-02-18 12:34:31 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PJT2502181
+{
+    class DynamicArray
+    {
+
+
+        void Add(Object inObject)
+        {
+            objects[CurrentPosition] = inObject;
+        }
+
+        Object[] objects = new object[10];
+        int CurrentPosition = 0;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
